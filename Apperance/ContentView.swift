7006fcecac6308154683aedd9616942cb63458d1
Alexander Ross <alex@aross.se>
2020-01-25 19:33:23 +0100

    Initial Commit

---
+++ Apperance/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Apperance
+//
+//  Created by Alexander Ross on 2020-01-25.
+//  Copyright © 2020 Alexander Ross. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ApperanceTests/ApperanceTests.swift
@@ -0,0 +1,34 @@
+//
+//  ApperanceTests.swift
+//  ApperanceTests
+//
+//  Created by Alexander Ross on 2020-01-25.
+//  Copyright © 2020 Alexander Ross. All rights reserved.
+//
+
+import XCTest
+@testable import Apperance
+
+class ApperanceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
